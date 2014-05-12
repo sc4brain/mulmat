2c9129e74ed0ca262acee3150779697b28bb42fc
heewonpark <park.brainlab@gmail.com>
2014-05-12 20:28:32 +0900

    @dekimasita

--- mulmat.c
+++ mulmat.c
@@ -5,9 +5,23 @@ void mulMat_park(const int matsize,
 	     const double *matb,
 	     double *matc)
 {
-  ;
-}
+  //  int s = matsize;
+  int i;
+  int j;
+  //  double *answer=0;
 
+  for(i=0; i<(matsize*matsize); i++){
+    *(matc+i)=0;
+  }
+  
+  for(i=0; i<(matsize*matsize); i++){
+    for(j=0; j<matsize; j++){
+      *(matc+i) = *(matc+i) + *(mata+ i - i%matsize + j) * *(matb + i % matsize + j*matsize);
+    }
+  }
+  //  matc = answer;
+
+}
 /***************** FUKUDA *******************/
 void mulMat_fukuda(const int matsize,
 	     const double *mata,
