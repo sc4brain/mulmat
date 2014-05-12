b3184b30503c6f3d9cbe43f0a2254f668b8ccf98
yamamarin <yamazaki@brain.imi.i.u-tokyo.ac.jp>
2014-05-12 20:18:45 +0900

    yamazaki kansei

--- mulmat.c
+++ mulmat.c
@@ -23,7 +23,25 @@ void mulMat_yamazaki(const int matsize,
 	     const double *matb,
 	     double *matc)
 {
-  ;
+  int i=0;
+  int row=0,col=0;
+  double bus=0.0;
+  int j=0,k=0;
+
+
+  for(row=0;row<matsize;row++){
+    for(col=0;col<matsize;col++){
+      k=col;
+      for(j=row*matsize;j<(row+1)*matsize;j++){
+	bus += mata[j]*matb[k];
+	k+=matsize;
+      }
+      matc[i]=bus;
+      i++;
+      bus = 0.0;
+    }
+  }
+
 }
 
 /***************** GOTO *******************/
